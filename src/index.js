3c5cfc2a56c68e5faa9b25804bcee5d06d13aad3
fredy-ascuntar <fredy_0793@hotmail.com.com>
2024-08-02 07:49:30 -0500

    inicio añadido

--- src/index.js
+++ src/index.js
@@ -1,35 +1,36 @@
-const express = require('express')
-const cors = require('cors')
-const connection = require('./model/database')
-const routeUsuario = require('./routes/routesUsuario').default
+const express = require("express");
+const cors = require("cors");
+const path = require("path");
+const connection = require("./model/database");
+const routeUsuario = require("./routes/routesUsuario").default;
 
 class Server {
+  constructor() {
+    this.app = express();
+    this.app.set("port", 4077);
+    this.app.use(express.json());
+    this.app.use(cors());
 
-    constructor() {
+    //
+    this.app.use(express.static(path.join(__dirname, "..", "..", "FrontEnd")));
+    //
 
-        this.app = express()
-        this.app.set('port', 4077)
-        this.app.use(express.json())
-        this.app.use(cors())
+    this.app.get("/", (req, res) => {
+      //res.status(200).json({ correcto: "Conectado" });
+      res.sendFile(path.join(__dirname, "..", "..", "FrontEnd", "index.html"));
+    });
+    this.app.use(new routeUsuario().ruta);
 
-        this.app.get('/', (req, res) => {
-            res.status(200).json({ correcto: "Conectado" })
-        })
-        this.app.use(new routeUsuario().ruta)
+    this.app.listen(4077, () => {
+      console.log("Corriendo en puerto " + 4077);
+    });
 
+    this.connectionBd;
+  }
 
-        this.app.listen(4077, () => {
-            console.log("Corriendo en puerto " + 4077)
-        })
-
-        this.connectionBd
-
-    }
-
-    async connectionBd() {
-        await connection
-    }
-
+  async connectionBd() {
+    await connection;
+  }
 }
 
-const run = new Server()
+const run = new Server();
